51321090bd8afdcd111d09d4d3b5f433dc3fbe84
Dominika <dominikajedynak8@interia.pl>
2026-04-21 21:33:04 +0200

    Uproszczenie modelu Person do podstawowego mapowania ODM

--- src/main/java/org/example/model/Person.java
+++ src/main/java/org/example/model/Person.java
@@ -1,20 +1,13 @@
 package org.example.model;
 
 import lombok.AllArgsConstructor;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-import java.util.List;
-
-@Getter
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
 public class Person {
     private String name;
     private int age;
-    private boolean active;
-    private Address address;
-    private List<String> interests;
 }
