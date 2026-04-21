897e34efba849038f46673c7b5c5cc26269b6fa0
Kacperrk <kacper0492@wp.pl>
2026-04-21 21:37:42 +0200

    Dodanie reflectionutils

--- src/main/java/org/example/util/ReflectionUtils.java
+++ src/main/java/org/example/util/ReflectionUtils.java
@@ -1,9 +1,34 @@
 package org.example.util;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.ArrayList;
+import java.util.List;
+
 public final class ReflectionUtils {
 
-    private ReflectionUtils() {
+    public static List<Field> getSerializableFields(Class<?> clazz) {
+        List<Field> fields = new ArrayList<>();
+
+        for (Field field : clazz.getDeclaredFields()) {
+            int modifiers = field.getModifiers();
+
+            if (Modifier.isStatic(modifiers) || Modifier.isTransient(modifiers) || field.isSynthetic()) {
+                continue;
+            }
+
+            fields.add(field);
+        }
+
+        return fields;
     }
 
-    // Klasa pomocnicza przygotowana pod późniejszą obsługę refleksji.
+    public static String escapeJson(String value) {
+        return value
+                .replace("\\", "\\\\")
+                .replace("\"", "\\\"")
+                .replace("\n", "\\n")
+                .replace("\r", "\\r")
+                .replace("\t", "\\t");
+    }
 }
